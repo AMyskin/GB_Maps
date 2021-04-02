aad4b80a030f18a6d231894c9cc96f3cc6b49c08
Aleksandr Myskin <aleksandr.myskin@bsc-ideas.com>
2021-04-03 00:01:51 +0300

    lesson 1

--- GB_Maps/ViewController.swift
+++ GB_Maps/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let coordinate = CLLocationCoordinate2D(latitude: 55.728899, longitude: 37.654048)
+    // Маркер:
+    var marker: GMSMarker?
+    var locationManager: CLLocationManager?
+
+    @IBOutlet weak var mapView: GMSMapView!
+
+    @IBAction func currentLocation(_ sender: UIButton) {
+        locationManager?.requestLocation()
+    }
+    
+    @IBAction func recordLocation(_ sender: UIButton) {
+        locationManager?.startUpdatingLocation()
     }
 
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 16)
+        mapView.camera = camera
+
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestAlwaysAuthorization()
+    }
+
+
+}
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        guard let location = locations.last else { return }
+        let position = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 16)
+        mapView.animate(to: position)
+        let marker = GMSMarker(position: location.coordinate)
+        marker.map = mapView
+        self.marker = marker
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
