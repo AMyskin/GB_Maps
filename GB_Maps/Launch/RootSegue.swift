a31e84f4f735beb32b92a1e5fb422f92488a4b86
Aleksandr Myskin <aleksandr.myskin@bsc-ideas.com>
2021-04-03 15:24:16 +0300

    UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = destination

--- GB_Maps/Launch/RootSegue.swift
+++ GB_Maps/Launch/RootSegue.swift
@@ -10,15 +10,17 @@ import UIKit
 class RootSegue: UIStoryboardSegue {
 
     override func perform() {
-        let keyWindow = UIApplication.shared.connectedScenes
-                .filter({$0.activationState == .foregroundActive})
-                .map({$0 as? UIWindowScene})
-                .compactMap({$0})
-                .first?.windows
-                .filter({$0.isKeyWindow}).first
-        keyWindow?.endEditing(true)
+//        let keyWindow = UIApplication.shared.connectedScenes
+//                .filter({$0.activationState == .foregroundActive})
+//                .map({$0 as? UIWindowScene})
+//                .compactMap({$0})
+//                .first?.windows
+//                .filter({$0.isKeyWindow}).first
+//        keyWindow?.endEditing(true)
+//
+//        keyWindow?.rootViewController = destination
 
-        keyWindow?.rootViewController = destination
+        UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = destination
 
         //UIApplication.shared.keyWindow?.rootViewController = destination
     }
